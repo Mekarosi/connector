0de998237b81dfa18fb276d4b6b980d0c48ee4c4
Midland digital <mekarosi@midlanddigitalsolutions.com>
2021-12-11 19:14:01 +0100

    Implement request body validation

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const app = express()
 // Connect Database
 connectDB()
 
+// Init Middleware
+app.use(express.json({ extended: false }))
+
+
 app.get('/', (req, res) => res.send('API Running'))
 
+// Define Routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
