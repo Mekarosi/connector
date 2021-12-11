2239817d88ce8c4e98b175044941293e80ed1822
Midland digital <mekarosi@midlanddigitalsolutions.com>
2021-12-11 19:51:01 +0100

    Implement JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
 const User = require('../../models/Users')
 
@@ -26,7 +28,7 @@ async (req, res) => {
 
         let user = await User.findOne({ email })
         if(user){
-            res.status(400).json({ errors: [ { msg: 'User already exist' } ] })
+           return res.status(400).json({ errors: [ { msg: 'User already exist' } ] })
         }
         //Get users gravatar
 
@@ -50,8 +52,19 @@ async (req, res) => {
     await user.save()
 
         // Return jsonwebtoken
-
-        res.send('User registered')
+    const payload = {
+        user: {
+            id: user.id
+        }
+    }
+    jwt.sign(
+        payload, 
+        config.get('jwtSecret'),
+        {expiresIn: 36000},
+        (err, token) => {
+           if(err) throw err
+           res.json({ token })    
+        }) 
 
    } catch (err) {
        console.error(err.message)
